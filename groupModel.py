baabde31295e0f4950f80c7a572e1015880c03ca
Na7hanL <98568997+Na7hanL@users.noreply.github.com>
2023-11-19 19:43:32 -0600

    Uploading models and scripts to test all data

---
+++ groupModel.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+from dataParser import *
+import time
+from sklearn.model_selection import train_test_split
+
+def makeGroupModel(interval):
+
+    print("Gathering Data... \n")
+    startTime = time.time()
+
+    (data, labels) = getAllData(interval)
+
+    data_train, data_test, labels_train, labels_test = train_test_split(data,labels , random_state=104,test_size=0.20, shuffle=True)
+
+    dataGatheredTime = time.time()
+
+    print("All data has been gathered... \n Time elapsed: ", dataGatheredTime-startTime, '\n')
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(128, 65)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128),
+        tf.keras.layers.Dense(2)
+    ])
+
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+    print("Model Compiled... \nStarting Training...\n")
+
+    model.fit(data_train, labels_train, epochs=10)
+
+    modelFitTime = time.time()
+
+    print("Model Trained... \nTime elapsed: ", modelFitTime - dataGatheredTime, '\n')
+
+    test_loss, test_acc = model.evaluate(data_test,  labels_test, verbose=2)
+
+    print('\nTest accuracy:', test_acc, '\n')
+
+    model.save(f'./results/result_interval{interval}_allParticipants_accuracy{round(test_acc, 3)*1000}.h5')
---
+++ testAllGroups.py
@@ -0,0 +1,10 @@
+from groupModel import makeGroupModel
+
+
+def main():
+    
+    for i in range(12):
+        makeGroupModel(i + 1)
+
+
+main()
